21d6a424ef3d03a8f1c929f945bcd32b222cac61
rihanmohammed26 <rihanmohammed26@gmail.com>
2021-09-05 00:39:25 +0530

    Create Api.js

---
+++ src/Components/Api.js
@@ -0,0 +1,12 @@
+const ApiData=[
+    {
+        id:1,
+        text:"This Is My First List"
+    },
+    {
+        id:2,
+        text:"This Is My Second List"
+    },
+];
+
+export default ApiData;
