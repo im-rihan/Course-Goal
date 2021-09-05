c6649f127b50314d1b93046c8a0c99a8a476bb7c
rihanmohammed26 <rihanmohammed26@gmail.com>
2021-09-05 13:01:35 +0530

    added form validation

--- src/Components/CourseGoalInput/CourseGoalInput.js
+++ src/Components/CourseGoalInput/CourseGoalInput.js
@@ -3,22 +3,33 @@ import "./CourseGoalInput.css";
 import Button from "../UI Buttons/Button";
 const CourseGoalInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputHandler = (event) => {
+    if (enteredValue.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
   const formSubmitHandler = (event) => {
     event.preventDefault();
     // console.log(props.onAddGoal(enteredValue));
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
+          style={{
+            border: !isValid ? "1px solid red" : "1px solid #ccc",
+            background: !isValid ? "salmon" : "transparent",
+          }}
           type="text"
-          placeholder="Add Goal"
           onChange={goalInputHandler}
           value={enteredValue}
         />
